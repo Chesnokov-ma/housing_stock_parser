5f6e014d9fe65fde04ce40ddd6429a741f493aee
grishasolakhyan <borsch.tv@mail.ru>
2024-03-02 23:57:46 +1000

    housing stock parser update

--- main (beta).py
+++ main (beta).py
@@ -9,6 +9,8 @@ import pandas as pd
 from collections import Counter
 import numpy
 import matplotlib.pyplot as plt
+from matplotlib.dates import date2num
+import matplotlib.patches as mpatches
 import seaborn as sns
 
 class Data_capture:
@@ -203,21 +205,80 @@ class Graphs:
     def __init__(self):
         pass
 
-    def bar_chart_hsp(self, data_hsp_column, discription, name_c):
+    def year_plot_hsp(self, data_hsp_column, discription, name_c):
+
         data_hsp_list = data_hsp_column.tolist()
         data_array_counts = dict(Counter(data_hsp_list))
 
         if 'Нет данных' in data_array_counts.keys():
-            del data_array_counts['Нет данных']
+             del data_array_counts['Нет данных']
+
+        df = pd.DataFrame(data_array_counts.items(), columns=['key', 'val'])
+        df = df.sort_values(by='key', ascending=True)
+
+        df['key'] = pd.to_datetime(df['key'])
+        df['key'] = df['key'].dt.year
+
+        text_y_pos = df['val'].max() + 0.5
+        period_colors = {'imperal_color': '#f2a46e',
+                         'civil_war_color': '#3b373a',
+                         'ussr_color': '#e3132a',
+                         'rf_color': '#4550a7'}
+
+        russian_civil_war = 1917
+        soviet = 1922
+        russian_federation = 1991
+
+        df['colors'] = df['key'].apply(
+        lambda x: period_colors['imperal_color'] if x < russian_civil_war
+        else period_colors['civil_war_color'] if x >= russian_civil_war and x < soviet
+        else period_colors['ussr_color'] if x >= soviet and x < russian_federation
+        else period_colors['rf_color'] if x >= russian_federation else 'gray')
+
+        fig, ax = plt.subplots(figsize=(8, 8))
+        plt.bar(df['key'], df['val'], color=df['colors'])
+
+        ax.axvline(x=russian_civil_war, zorder=0, color='grey', ls='--', lw=1.5)
+        ax.text(x=russian_civil_war, y=text_y_pos, s='1917г.', ha='center',
+                fontsize=10, bbox=dict(facecolor='white', edgecolor='grey'))
+
+        ax.axvline(x=soviet, zorder=0, color='grey', ls='--', lw=1.5)
+        ax.text(x=soviet, y=text_y_pos, s='1922г.', ha='center',
+                fontsize=10, bbox=dict(facecolor='white', edgecolor='grey'))
+
+        ax.axvline(x=russian_federation, zorder=0, color='grey', ls='--', lw=1.5)
+        ax.text(x=russian_federation, y=text_y_pos, s='1991г.', ha='center',
+                fontsize=10, bbox=dict(facecolor='white', edgecolor='grey'))
+
+        imper_patch = mpatches.Patch(color=period_colors['imperal_color'], label='Российская империя')
+        civil_war_patch = mpatches.Patch(color=period_colors['civil_war_color'], label='Гражданская война')
+        soviet_patch = mpatches.Patch(color=period_colors['ussr_color'], label='СССР')
+        rf_patch = mpatches.Patch(color=period_colors['rf_color'], label='Российская Федерация')
+        ax.legend(handles=[imper_patch, civil_war_patch, soviet_patch, rf_patch])
+
+        ax.set_title('Диаграмма распределения домов (г. ' + name_c + ')')
+        ax.set_xlabel(discription)
+        plt.show()
+
+        return 0
+
+    def bar_chart_hsp(self, data_hsp_column, discription, name_c):
+
+        data_hsp_list = data_hsp_column.tolist()
+        data_array_counts = dict(Counter(data_hsp_list))
+
+        if 'Нет данных' in data_array_counts.keys():
+             del data_array_counts['Нет данных']
 
         df = pd.DataFrame(data_array_counts.items(), columns = ['key', 'val'])
         df = df.sort_values(by = 'val', ascending = True)
 
         fig, ax = plt.subplots(figsize = (8,8))
-        bars = plt.barh(df['key'], df['val'], color = '#4f65ad')
+        bars = plt.bar(df['key'], df['val'], color = '#4f65ad')
         ax.bar_label(bars, color = 'black', fontsize = 10, label_type = 'edge', fontweight = 'bold')
         ax.set_title('Диаграмма распределения домов (г. '+ name_c+')')
         ax.set_ylabel(discription)
+        plt.xticks(rotation = 45)
         plt.show()
 
         return 0
@@ -250,7 +311,7 @@ class Summon_operations:
         src = stock.get_html(r_src, u_name)
         soup = BeautifulSoup(src, "lxml")
         all_pages_dict = stock.get_all_pages(soup, city_url)
-        stock.get_data(all_pages_dict, 1, u_name)
+        stock.get_data(all_pages_dict, 10, u_name)
 
         end_time = time.time() - start_time
         end_time = round(end_time, 3)
@@ -273,7 +334,7 @@ class Summon_operations:
 
         if (num_op_2 == '1'):
             print(f'Диаграмма по параметру: {num_operation_dict_2[num_op_2]}\n')
-            data_graph.bar_chart_hsp(data_hsp['Год постройки'], 'Год постройки',
+            data_graph.year_plot_hsp(data_hsp['Год постройки'], 'Год постройки',
                                      c_name)  # вызов метода столбчатой диаграммы по году постройки домов
         elif (num_op_2 == '2'):
             print(f'Диаграмма по параметру: {num_operation_dict_2[num_op_2]}\n')
@@ -314,12 +375,6 @@ class Summon_operations:
             for k, v, in city_dict.items(): print(f'г. {k}')
             print('0) Назад\n')
             replace_city_name = input('Введите название города для сбора данных по его жилищному фонду: ')
-            # replace_city_name = input('г. Москва\n'
-            #                           'г. Санкт-Петербург\n'
-            #                           'г. Казань\n'
-            #                           'г. Владивосток\n'
-            #                           '0) Назад\n\n'
-            #                           'Введите название города для сбора данных по его жилищному фонду: ')
 
             if replace_city_name == '0':
                 tmd = True
